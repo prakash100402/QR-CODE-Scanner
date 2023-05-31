c002c8743f4d410c187cc3980f7855f9bc016d8e
prakash100402 <avenjer2002@gmail.com>
2023-05-31 22:18:41 +0530

    QR Scanner

---
+++ app/src/main/java/com/example/tlesqrcodescanner/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.tlesqrcodescanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    public static TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+        txt = findViewById(R.id.textView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity2.class));
+            }
+        });
+
+    }
+}
